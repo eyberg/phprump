d1e6962d0d6a3153e0c3f886c499e94cfdad4e57
ian eyberg <ian@deferpanic.com>
2016-05-25 08:23:04 -0700

    .

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ Hello, <?php echo $_SERVER['REMOTE_ADDR']; ?>
 It is now <?php echo date(DATE_RFC2822); ?>
 <p>
 Served to you by
-<img src="http://204.12.231.130/img/logo.svg"/>
+<img src="htts://deferpanic/img/logo.svg"/>
 
 <p>
 Try <a href="phpinfo.php">phpinfo()</a>.
